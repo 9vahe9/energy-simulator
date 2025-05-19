dca39e603ac769434e0a0ccf8d284ad9a5c1d5b0
unknown <shushogrig91@gmail.com>
2025-05-19 19:04:55 +0400

    fix routing conglict

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { DASHBOARD_PATH, HOME_PATH, ROOM_PATH } from "../constants/RoutePaths";
+import {
+  DASHBOARD_PATH,
+  HOME_PATH,
+  ROOM_PATH,
+  LOGIN_PATH,
+} from "../constants/RoutePaths";
 import { ContentContainer } from "../components/ContentContainer";
 import HomePage from "../pages/HomePage";
 import DassboardPage from "../pages/DassboardPage";
 import RoomPage from "../pages/RoomPage";
 import { LoginContainer } from "../components/login/LoginContainer";
-
 
 export const router = createBrowserRouter([
   {
@@ -29,6 +33,6 @@ export const router = createBrowserRouter([
   },
   {
     path: LOGIN_PATH,
-    element: <LoginContainer />
-  }
+    element: <LoginContainer />,
+  },
 ]);
