c16d809aa810f361966bc80eddb320579a41fea6
unknown <shushogrig91@gmail.com>
2025-06-01 16:07:27 +0400

    add package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+//import ThreeScene from "./components/ThreeScene/SceneContainer";
 
 function App() {
   return <RouterProvider router={router} />;
