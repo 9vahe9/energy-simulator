12f8c3b3e670adc8cdad74edad3112f7ae5439b4
Tigran <mr.tigran.aslanyan@gmail.com>
2025-06-03 10:55:04 +0400

    Merge branch 'dev' of https://github.com/9vahe9/energy-simulator into dev

--- src/components/dashboard/DashboardContainer.tsx
+++ src/components/dashboard/DashboardContainer.tsx
@@ -11,8 +11,10 @@ import {
   Space,
   Progress,
   Popconfirm,
+
   Modal,
   InputNumber,
+
 } from "antd";
 import type { RootState, AppDispatch } from "../../store/store";
 import { useSelector, useDispatch } from "react-redux";
@@ -39,7 +41,7 @@ import Search from "antd/es/transfer/search";
 
 export const DashboardContainer: React.FC = () => {
   const { Title, Text } = Typography;
-  const { singleRoomPage, handleAddingRoom } = useAddRooms();
+  const { singleRoomPage, handleAddingRoom, setRoomData } = useAddRooms();
 
   let roomsArray = useSelector((state: RootState) => state.user.rooms);
   const userName = useSelector((state: RootState) => state.user.userName);
@@ -55,21 +57,29 @@ export const DashboardContainer: React.FC = () => {
 
   const filteredRooms = roomsArray.filter((room) => {
     return room.name.toLowerCase().includes(userSearch.toLowerCase());
-
   });
+ dev
   const showModal = () => {
     setModalVisible(true);
   };
-  const handleOk = () => {
-    handleAddingRoom();
-    setModalVisible(false);
-    form.resetFields();
+  const handleOk = async () => {
+    try {
+      const values = await form.validateFields();
+      setRoomData(values.name, values.description || "");
+      await handleAddingRoom();
+      setModalVisible(false);
+      form.resetFields();
+    } catch (error) {
+      console.error("Validation failed:", error);
+    }
   };
 
   const handleCancel = () => {
     setModalVisible(false);
     form.resetFields();
   };
+
+
 
   useEffect(() => {
     if (userID) {
@@ -97,78 +107,11 @@ export const DashboardContainer: React.FC = () => {
   return (
     <div className="dashboard-container">
 
-      {userName}
-      <div className="dashboard-header">
-        <div className="header-left">
-          <Title level={3} style={{ margin: 0 }}>
-            Room Energy Management
-          </Title>
-          <Button type="primary" onClick={() => showModal()}>
-            Add new room
-          </Button>
-          <Modal
-            title="add device"
-            open={modalVisible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            okText="Add"
-          >
-            <Form form={form} layout="vertical">
-              <Form.Item
-                name="name"
-                label="Name"
-                rules={[
-                  { required: true, message: "Please enter room name" },
-                  { min: 3, message: "min 3 charachter" },
-                ]}
-              >
-                <Input minLength={3} maxLength={15} />
-              </Form.Item>
-              <Form.Item
-                name="description"
-                label="Description"
-                rules={[
-                  { required: false, message: "Please enter room description" },
-                  { min: 3, message: "min 150 charachter" },
-                ]}
-              >
-                <Input minLength={3} maxLength={150} />
-              </Form.Item>
-            </Form>
-          </Modal>
-          <Button icon={<SortAscendingOutlined />} style={{ marginLeft: 12 }}>
-            Sort Rooms
-          </Button>
-        </div>
-
-        <div className="header-right">
-          <div className="header-summary">
-            <Text>Total Energy Consumption</Text>
-            <Title level={4} style={{ margin: 0 }}>
-              {/* {totalEnergy.toLocaleString()} kWh */}
-            </Title>
-            <Text>Monthly Cost</Text>
-            <Title level={4} style={{ margin: 0 }}>
-              {/* ${totalCost.toFixed(2)} */}
-            </Title>
-          </div>
-          <div className="header-search">
-            <Input
-              placeholder="Search rooms..."
-              prefix={<SearchOutlined />}
-              value={userSearch}
-              onChange={(e) => setUserSearch(e.target.value)}
-            />
-
-
-
-        <Title level={2}>
-          Room Energy Management
-        </Title>
-        <div className="wrapper">
-          <Row className="dashboard-header" justify="space-between">
-            <Col className="header-left">
-              <Space>
+      <Title level={2}>Room Energy Management</Title>
+      <div className="wrapper">
+        <Row className="dashboard-header" justify="space-between">
+          <Col className="header-left">
+            <Space>
               <Text italic style={{ margin: 0 }}>
                 {userName}
               </Text>
@@ -176,74 +119,98 @@ export const DashboardContainer: React.FC = () => {
                 type="primary"
                 icon={<PlusOutlined />}
                 style={{ marginLeft: 16 }}
-                onClick={() => navigate(ROOM_PATH)}
-
+                onClick={() => showModal()}
                 className="add-room-button"
-
               >
                 Add New Room
               </Button>
-              <Button icon={<SortAscendingOutlined />} style={{ marginLeft: 12 }}>
+              <Modal
+                title="add device"
+                open={modalVisible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                okText="Add"
+              >
+                <Form form={form} layout="vertical">
+                  <Form.Item
+                    name="name"
+                    label="Name"
+                    rules={[
+                      { required: true, message: "Please enter room name" },
+                      { min: 3, message: "min 3 charachter" },
+                    ]}
+                  >
+                    <Input minLength={3} maxLength={15} />
+                  </Form.Item>
+                  <Form.Item
+                    name="description"
+                    label="Description"
+                    rules={[
+                      {
+                        required: false,
+                        message: "Please enter room description",
+                      },
+                      { min: 3, message: "min 150 charachter" },
+                    ]}
+                  >
+                    <Input minLength={3} maxLength={150} />
+                  </Form.Item>
+                </Form>
+              </Modal>
+              <Button
+                icon={<SortAscendingOutlined />}
+                style={{ marginLeft: 12 }}
+              >
                 Sort Rooms
               </Button>
-              </Space>
-            </Col>
-
-            
-              <Col className="header-summary">
-                <Space>
-                <Text>Total Energy Consumption։</Text>
-                <Title level={4} style={{ margin: 0 }}>
-                  {/* {totalEnergy} */}
-                </Title>
-                <Text>Monthly Cost։</Text>
-                <Title level={4} style={{ margin: 0 }}>
-                  {/* {totalCost} */}
-                </Title>
-                </Space>
-              </Col>
-              <Col className="header-right">
-                <Search
-                  placeholder="Search rooms..."
-                  value={userSearch}
-                  onChange={(e) => setUserSearch(e.target.value)}
-                />
-              </Col>
-          </Row >
-
-      </div>
-      
-      <div className="wrapper">
-        <Row gutter={[24, 24]}>
-        {filteredRooms.length > 0 && filteredRooms.map((room) => (
-          room.name !== " " &&
-          <Col 
-            xs={24}
-            sm={12}
-            xl={6}
-          >
-            <RoomCards
-              key={room.id}
-              name={room.name}
-              id={room.id}
-              priority={room.energyConsumption}
-              energy={room.levelOfEnergyConsumption}
-              icons={room.devices}
-              cost={room.monthlyCost}
-              deleteFunction={handleDelete}
-              editRoomFunction={handleEditRoom}
-
+            </Space>
+          </Col>
+          <Col className="header-summary">
+            <Space>
+              <Text>Total Energy Consumption։</Text>
+              <Title level={4} style={{ margin: 0 }}>
+                {/* {totalEnergy} */}
+              </Title>
+              <Text>Monthly Cost։</Text>
+              <Title level={4} style={{ margin: 0 }}>
+                {/* {totalCost} */}
+              </Title>
+            </Space>
+          </Col>
+          <Col className="header-right">
+            <Search
+              placeholder="Search rooms..."
+              value={userSearch}
+              onChange={(e) => setUserSearch(e.target.value)}
             />
           </Col>
-        ))}
-      </Row>
-
+        </Row>
+      </div>
+      <div className="wrapper">
+        <Row gutter={[24, 24]}>
+          {filteredRooms.length > 0 &&
+            filteredRooms.map(
+              (room) =>
+                room.name !== " " && (
+                  <Col xs={24} sm={12} xl={6}>
+                    <RoomCards
+                      key={room.id}
+                      name={room.name}
+                      id={room.id}
+                      priority={room.energyConsumption}
+                      energy={room.levelOfEnergyConsumption}
+                      icons={room.devices}
+                      cost={room.monthlyCost}
+                      deleteFunction={handleDelete}
+                      editRoomFunction={handleEditRoom}
+                    />
+                  </Col>
+                )
+            )}
+        </Row>
       </div>
 
       <Button onClick={handleLogOut}>Log out</Button>
       </div>
-    </div>
-  </div>
-  </div>
   );
 };
