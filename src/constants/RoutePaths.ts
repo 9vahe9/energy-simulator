d1e6c419bfc43fe2ad016fbb76368a5fc1aa9130
Hamlet Darmoyan <darmoyanham@gmail.com>
2025-06-06 16:22:43 +0400

    report page skeleton

--- src/constants/RoutePaths.ts
+++ src/constants/RoutePaths.ts
@@ -3,5 +3,5 @@ export const LOGIN_PATH = "/login";
 export const SIGNUP_PATH = "/signup";
 export const DASHBOARD_PATH = "/dashboard";
 export const ROOM_PATH = "/room";
+export const REPORT_PATH = "/report";
 export const NOT_FOUND_PATH = "*";
-
