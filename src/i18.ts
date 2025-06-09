c3fae16d1339d32f947eb953512a0018ffe38220
Hamlet Darmoyan <darmoyanham@gmail.com>
2025-06-09 20:52:24 +0400

    completed translation in dashboard and report pages, and some canges in dashboard

--- src/i18.ts
+++ src/i18.ts
@@ -1,9 +1,10 @@
 import i18n from "i18next";
-import { initReactI18next, Translation } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 
 import TranslationEn from "./locals/en/translation.json";
 import TranslationRu from "./locals/ru/translation.json";
 import TranslationAm from "./locals/am/translation.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 const resources = {
     en: {
@@ -18,13 +19,17 @@ const resources = {
 };
 
 i18n
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources,
-        lng: "en",
         fallbackLng: "en",
         interpolation: {
             escapeValue: false,
+        },
+        detection: {
+            order: ["localStorage"],
+            caches: ["localStorage"],
         },
         debug: true,
     });
